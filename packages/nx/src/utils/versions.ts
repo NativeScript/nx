57a0f0b15cd864a8ec32a2657b0941b794b1a45a
Nathan Walker <walkerrunpdx@gmail.com>
2021-03-31 23:01:06 -0700

    chore: bump versions

--- packages/nx/src/utils/versions.ts
+++ packages/nx/src/utils/versions.ts
@@ -1,10 +1,10 @@
 export const nxVersion = '~11.5.0';
 
 export const nsNxPluginVersion = '~1.0.0';
-export const nsCoreVersion = '8.0.0-alpha.8';
-export const nsTypesVersion = '~7.3.0';
+export const nsCoreVersion = '~8.0.0';
+export const nsTypesVersion = '~8.0.0';
 export const nsThemeVersion = '~1.0.4';
-export const nsWebpackVersion = 'alpha';
+export const nsWebpackVersion = '~5.0.0-beta.0';
 export const sassVersion = '~1.32.0';
 
 // Frontend frameworks
