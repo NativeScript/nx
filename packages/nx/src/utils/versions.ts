9a0299848a7497581d2576ee99ca748b54ce6e73
Nathan Walker <walkerrunpdx@gmail.com>
2021-06-24 18:35:10 -0700

    feat: dep updates

--- packages/nx/src/utils/versions.ts
+++ packages/nx/src/utils/versions.ts
@@ -4,7 +4,7 @@ export const nsNxPluginVersion = '~2.0.0';
 export const nsCoreVersion = '~8.0.0';
 export const nsTypesVersion = '~8.0.0';
 export const nsThemeVersion = '~1.0.4';
-export const nsWebpackVersion = 'dev';
+export const nsWebpackVersion = 'beta';
 export const sassVersion = '^1.32.0';
 export const nsIOSRuntimeVersion = '8.0.0';
 export const nsAndroidRuntimeVersion = '8.0.0';
@@ -12,7 +12,7 @@ export const nsAndroidRuntimeVersion = '8.0.0';
 
 // Frontend frameworks
 export const angularVersion = '~12.0.0';
-export const nsAngularVersion = 'alpha';
+export const nsAngularVersion = '~12.0.0';
 export const nsNgToolsVersion = '~12.0.0';
 export const rxjsVersion = '~6.6.0';
 export const zonejsVersion = '^0.11.1';
