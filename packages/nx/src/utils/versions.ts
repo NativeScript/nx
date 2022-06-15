9c27f3c594cf9c1bbf3b3ea2397e1dd8d864aa1a
Nathan Walker <walkerrunpdx@gmail.com>
2022-06-15 11:14:46 -0700

    feat: angular 14

--- packages/nx/src/utils/versions.ts
+++ packages/nx/src/utils/versions.ts
@@ -1,19 +1,19 @@
-export const nxVersion = '^13.0.0';
+export const nxVersion = '^14.0.0';
 
-export const nsNxPluginVersion = '~3.0.0';
-export const nsCoreVersion = '~8.1.0';
-export const nsTypesVersion = '~8.1.0';
+export const nsNxPluginVersion = '~4.0.0';
+export const nsCoreVersion = '~8.2.0';
+export const nsTypesVersion = '~8.2.0';
 export const nsThemeVersion = '~1.0.4';
 export const nsWebpackVersion = '~5.0.0';
 export const sassVersion = '^1.32.0';
-export const nsIOSRuntimeVersion = '~8.1.0';
-export const nsAndroidRuntimeVersion = '~8.1.1';
+export const nsIOSRuntimeVersion = '~8.2.0';
+export const nsAndroidRuntimeVersion = '~8.2.1';
 
 
 // Frontend frameworks
-export const angularVersion = '^13.0.0';
-export const nsAngularVersion = '^13.0.0';
-export const nsNgToolsVersion = '^13.0.0';
+export const angularVersion = '^14.0.0';
+export const nsAngularVersion = '^14.0.0';
+export const nsNgToolsVersion = '^14.0.0';
 export const rxjsVersion = '~7.5.0';
 export const zonejsVersion = '^0.11.4';
-export const typescriptVersion = '~4.5.0';
+export const typescriptVersion = '~4.7.0';
