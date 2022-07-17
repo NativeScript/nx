da7c430b0aeb61af91f2f553399ea4d7980cf7bf
Nathan Walker <walkerrunpdx@gmail.com>
2022-07-16 20:49:48 -0700

    chore: cleanup

--- packages/nx/src/executors/test/executor.ts
+++
@@ -1,7 +0,0 @@
-import { ExecutorContext } from '@nrwl/devkit';
-import { commonExecutor } from '../../utils';
-import { ExecutorSchema } from '../../utils/types';
-
-export default async function testExecutor(options: ExecutorSchema, context: ExecutorContext) {
-  return commonExecutor(options, context);
-}
