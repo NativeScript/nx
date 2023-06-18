5836a24d6625d3e719474cc339cf408786d9798e
Nathan Walker <walkerrunpdx@gmail.com>
2023-06-17 18:22:40 -0700

    chore: Nx 16.3

--- packages/nx/src/utils/versions.ts
+++ packages/nx/src/utils/versions.ts
@@ -1,6 +1,6 @@
-export const nxVersion = '^15.0.0';
+export const nxVersion = '^16.3.0';
 
-export const nsNxPluginVersion = '^4.0.0';
+export const nsNxPluginVersion = '^16.3.0';
 export const nsCoreVersion = '~8.5.0';
 export const nsTypesVersion = '~8.5.0';
 export const nsThemeVersion = '~1.0.4';
@@ -9,11 +9,10 @@ export const sassVersion = '^1.32.0';
 export const nsIOSRuntimeVersion = '~8.5.0';
 export const nsAndroidRuntimeVersion = '~8.5.0';
 
-
 // Frontend frameworks
-export const angularVersion = '^15.0.0';
-export const nsAngularVersion = '^15.0.0';
-export const nsNgToolsVersion = '^15.0.0';
+export const angularVersion = '^16.0.0';
+export const nsAngularVersion = '^16.0.0';
+export const nsNgToolsVersion = '^16.0.0';
 export const rxjsVersion = '~7.8.0';
 export const zonejsVersion = '^0.13.0';
-export const typescriptVersion = '~4.9.0';
+export const typescriptVersion = '~5.0.0';
