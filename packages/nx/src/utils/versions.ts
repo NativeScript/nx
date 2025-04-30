1a3e61984f734704c0b2562219f38957a3554025
Nathan Walker <walkerrunpdx@gmail.com>
2025-04-29 19:03:33 -0700

    feat: NativeScript 8.9

--- packages/nx/src/utils/versions.ts
+++ packages/nx/src/utils/versions.ts
@@ -1,11 +1,11 @@
 export const nxVersion = '^20.0.0';
 
 export const nsNxPluginVersion = '^20.0.0';
-export const nsCoreVersion = '~8.8.0';
-export const nsTypesVersion = '~8.8.0';
+export const nsCoreVersion = '~8.9.0';
+export const nsTypesVersion = '~8.9.0';
 export const nsWebpackVersion = '~5.0.0';
-export const nsIOSRuntimeVersion = '~8.8.0';
-export const nsAndroidRuntimeVersion = '~8.8.0';
+export const nsIOSRuntimeVersion = '~8.9.0';
+export const nsAndroidRuntimeVersion = '~8.9.0';
 export const nsTailwindVersion = '^2.1.0';
 export const tailwindVersion = '~3.4.0';
 export const ajvVersion = '~8.17.0';
@@ -16,4 +16,4 @@ export const nsAngularVersion = '^19.0.0';
 export const nsNgToolsVersion = '^19.0.0';
 export const rxjsVersion = '~7.8.0';
 export const zonejsVersion = '^0.15.0';
-export const typescriptVersion = '~5.6.0';
+export const typescriptVersion = '~5.7.0';
