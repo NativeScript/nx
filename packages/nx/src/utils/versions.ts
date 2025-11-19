04940bafd9e6e56274806e3cada077e8f2d1879a
Nathan Walker <walkerrunpdx@gmail.com>
2025-11-19 15:08:34 -0800

    feat: Nx 22.1, NativeScript 9, Ng 20+

--- packages/nx/src/utils/versions.ts
+++ packages/nx/src/utils/versions.ts
@@ -1,19 +1,19 @@
 export const nxVersion = '^22.0.0';
 
 export const nsNxPluginVersion = '^22.0.0';
-export const nsCoreVersion = '~8.9.0';
-export const nsTypesVersion = '~8.9.0';
+export const nsCoreVersion = '~9.0.0';
+export const nsTypesVersion = '~9.0.0';
 export const nsWebpackVersion = '~5.0.0';
-export const nsIOSRuntimeVersion = '~8.9.0';
-export const nsAndroidRuntimeVersion = '~8.9.0';
+export const nsIOSRuntimeVersion = '~9.0.0';
+export const nsAndroidRuntimeVersion = '~9.0.0';
 export const nsTailwindVersion = '^2.1.0';
 export const tailwindVersion = '~3.4.0';
 export const ajvVersion = '~8.17.0';
 
 // Frontend frameworks
-export const angularVersion = '^19.0.0';
-export const nsAngularVersion = '^19.0.0';
-export const nsNgToolsVersion = '^19.0.0';
+export const angularVersion = '^20.0.0';
+export const nsAngularVersion = '^20.0.0';
+export const nsNgToolsVersion = '^20.0.0';
 export const rxjsVersion = '~7.8.0';
 export const zonejsVersion = '^0.15.0';
-export const typescriptVersion = '~5.7.0';
+export const typescriptVersion = '~5.8.0';
